bd5c375ce683254ace5975a7bf63588034e677ac
kidchang93 <lck930721@gmail.com>
2025-10-17 16:04:24 +0900

    결과창 수정 및 추가 기능을 위한 UI 제작

--- lib/views/result_view_widget.dart
+++ lib/views/result_view_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kcalai/buttons/custom_button.dart';
+import 'package:kcalai/models/button_model.dart';
 import 'package:kcalai/home.dart';
 import 'package:kcalai/models/prediction.dart';
 import 'package:kcalai/screen/main_screen.dart';
@@ -33,18 +35,45 @@ class _ResultWidgetState extends State<ResultWidget> {
 
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 16),
-          child: Text(
-              "해당 음식은 ${(topPrediction.score * 100).toStringAsFixed(2) + '%'}로 ${topPrediction.label}(으)로 보여집니다.\n"
-              "${topPrediction.label}의 칼로리와 영양 정보를\n"
-              "알려 드릴까요?",
-            style: AppTextStyles.body,
-            textAlign: TextAlign.center,
-          ),
+          child: Column(
+            children: [
+              Text(
+                "해당 음식은 ${(topPrediction.score * 100).toStringAsFixed(2) + '%'}로 ${topPrediction.label}(으)로 보여집니다.\n"
+                    "${topPrediction.label}의 칼로리와 영양 정보를\n"
+                    "알려 드릴까요?",
+                style: AppTextStyles.body,
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomButton(
+                        model: ButtonModel(
+                          text: "네",
+                          textColor: Colors.white,
+                          backgroundColor: Colors.green,
+                          borderRadius: 12,
+                          onPressed: () {
+                            // 버튼 클릭 시 행동
+                            print("영양 정보 보기 버튼 클릭");
+                          },
+                          icon: Icons.info_outline,
+                          isExpanded: false, // 필요하면 true
+                        ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
         ),
-        Column(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,  // Row 내부 요소를 가운데 정렬
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 8),
               child: ElevatedButton(onPressed: () {
                 // 다시 촬영 화면으로 이동
                 Navigator.push(
