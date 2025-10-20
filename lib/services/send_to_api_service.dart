d47bca0f5f758843973878acf599503e4df2bc5b
kidchang93 <lck930721@gmail.com>
2025-10-20 12:22:46 +0900

    버튼 기능 분리

---
+++ lib/services/send_to_api_service.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:kcalai/screen/result_screen.dart';
+import 'package:kcalai/services/api_service.dart';
+
+class SendToAPIService {
+  static Future<void> sendPhotoToAPI( BuildContext context, XFile imageFile ) async {
+    try {
+
+      final predictions = await ApiService.uploadPhoto(
+        File(imageFile.path),
+      );
+
+      if (!context.mounted) return; // ✅ context 안전성 보장
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              ResultScreen(predictions: predictions),
+        ),
+      );
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
+}
